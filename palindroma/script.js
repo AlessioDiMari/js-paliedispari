a12aa55dd4005d931ae062f1c8444de8602f456a
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-12 18:47:24 +0100

    creo l'html e inizio a scrivere la funzione di verifica

---
+++ palindroma/script.js
@@ -0,0 +1,15 @@
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// Inizializzo l'input
+const userWord = document.getElementById("user-word");
+
+// Creo una funzione che al click del button verifichi se
+// la parola inserita sia palindroma
+
+function paliVerify() {
+    
+    // memorizzo la parola inserita nell'input
+    let word = userWord.value;
+    
+}
