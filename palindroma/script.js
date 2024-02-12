5dadd64687c468eb52a508d01d4d0db0d7210c93
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-12 19:17:39 +0100

    creo il ciclo inverso per scrivere la seconda parola con in caratteri invertiti

--- palindroma/script.js
+++ palindroma/script.js
@@ -6,10 +6,24 @@ const userWord = document.getElementById("user-word");
 
 // Creo una funzione che al click del button verifichi se
 // la parola inserita sia palindroma
+const buttonVerify = document.getElementById("verify");
 
-function paliVerify() {
-    
+buttonVerify.addEventListener("click", function(){
+
     // memorizzo la parola inserita nell'input
     let word = userWord.value;
+
+    // Inizializzo una variaile dove inserirò le
+    // lettere della variabile nell'ordine inverso
+    let flippedWord = "";
+
+    // Creo un ciclo inverso per inserire le lettere della
+    // della parola inserite dall'utente nella nuova variabile
+    for ( let i = word.length - 1; i >= 0 ; i--){
+
+        flippedWord += word[i];
+        
+    }
     
-}
+    
+})
